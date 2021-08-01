b04f10548dae7985a987d67cd5fec079934cce8a
LYNY0103 <lyny7894@gmail.com>
2021-08-01 11:19:51 +0700

    ng class, ng For, ng If

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,16 @@ export class HomeComponent implements OnInit {
   public name:String = 'Hiep';
   public age = 15;
   public traicay = ['tao','nho','xoai'];
+  public traicay2 = [
+    {ten: 'táo', gia: 10,   giamgia: true},
+    {ten: 'nho', gia: -15,  giamgia: false},
+    {ten: 'xoài',gia: 20,   giamgia: false},
+    {ten: 'ổi',  gia: -115, giamgia: true}
+  ];
   constructor() { }
 
   public ngOnInit(): void {
-    console.log('trai cay = ', this.traicay);
+    console.log('trai cay = ', this.traicay2);
   }
 
 }
